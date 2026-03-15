47bd1ad008ddeee894a09d0b04608023d97c67c6
BRIAN EDUARDO CRUZ OLMEDO <brianeduardo1013@gmail.com>
2026-03-15 13:59:51 -0500

    Agregar archivos de proyecto.

---
+++ Polaris/Data/TiposProyectos.cs
@@ -0,0 +1,18 @@
+﻿namespace Polaris.Data
+{
+    public enum TiposProyectos
+    {
+        PolariX,
+        Mentes_en_Órbita,
+        Hydronautas
+    }
+    public class NameTiposProyectos
+    {
+        public TiposProyectos tiposProyectos {  get; set; }
+        public string ToString()
+        {
+            return tiposProyectos.ToString().Replace("_", " ");
+        }
+
+    }
+}
---
+++ Polaris/Modelo/UsuarioRegister.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Polaris.Modelo
+{
+    public class UsuarioRegister
+    {
+
+        [DisplayName("Nombre")]
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [MinLength(10, ErrorMessage = "El nombre debe tener al menos 10 caracteres")]
+        public string Name { get; set; } = "";
+
+        [DisplayName("Correo Electronico")]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        [EmailAddress(ErrorMessage = "El formato del correo no es válido")]
+        public string Email { get; set; } = "";
+
+        [DisplayName("Contraseña")]
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+
+        [MinLength(8, ErrorMessage = "Debe de ser mas de 8 caracteres")]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=(?:.*\d){2,})(?=.*[#$%^&*()_+\-=\[\]{};':""\\|,.<>\/?!]).{8,}$",
+    ErrorMessage = "La contraseña debe tener al menos: 1 mayúscula, 1 minúscula, 2 números y un carácter especial.")]
+        public string Password { get; set; } = "";
+
+        [DisplayName("Repite la Contraseña")]
+        [NotMapped]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Debes repetir la contraseña")]
+        [Compare("Password", ErrorMessage = "Las contraseñas no coinciden")]
+        [MinLength(8, ErrorMessage = "Debe de ser mas de 8 caracteres")]
+        public string RPassword { get; set; } = "";
+
+        [DisplayName("Proyecto de Interes")]
+        [Required(ErrorMessage = "Por favor, selecciona un proyecto")]
+        public int Proyecto { get; set; } = 0;
+    }
+}
